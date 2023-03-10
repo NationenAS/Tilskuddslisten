167b0fe55e06db9161ae75a63f81132e5bd73c01
BrukbareNationen <57347469+BrukbareNationen@users.noreply.github.com>
2023-03-10 10:20:37 +0100

    2022 update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var numberWithCommas = function (x) {
+  if(x == null) return 0;
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 };
 
@@ -6,29 +7,29 @@ function toggleInfo(e) {
 
   let orgnr = e.dataset.orgnr;
 
-  get2020(e, orgnr).then((data) => {
+  get2021(e, orgnr).then((data) => {
     if (data.entries.length > 1) { console.log("får flere treff. noe feil har skjedd"); return }
 
     if (data.entries.length == 0) {
-      e.querySelector('.lab-til__tilskudd2020').innerHTML = `<div class="lab-til__tilskudd2020">Sum tilskudd 2020: <span > 0 kr </span></div>`;
-      e.querySelector('.lab-til__avløser2020').innerHTML = `<div class="lab-til__avløser2020">Hvorav avløsertilskudd 2020: <span>0 kr</span></div>`;
+      e.querySelector('.lab-til__tilskudd2020').innerHTML = `<div class="lab-til__tilskudd2020">Sum tilskudd 2021: <span > 0 kr </span></div>`;
+      e.querySelector('.lab-til__avløser2020').innerHTML = `<div class="lab-til__avløser2020">Hvorav avløsertilskudd 2021: <span>0 kr</span></div>`;
 
       return
     }
 
     if (data.entries.length == 1) {
-      e.querySelector('.lab-til__tilskudd2020').innerHTML = `<div class="lab-til__tilskudd2020">Sum tilskudd 2020:  <span >  ${numberWithCommas(data.entries[0].sum_produksjons_og_avloesertilskudd)} kr </span></div>`;
-      e.querySelector('.lab-til__avløser2020').innerHTML = `<div class="lab-til__avløser2020">Hvorav avløsertilskudd 2020: <span>${numberWithCommas(data.entries[0].avloesertilskudd)} kr</span></div>`;
+      e.querySelector('.lab-til__tilskudd2020').innerHTML = `<div class="lab-til__tilskudd2020">Sum tilskudd 2021:  <span >  ${numberWithCommas(data.entries[0].sum_produksjons_og_avloesertilskudd)} kr </span></div>`;
+      e.querySelector('.lab-til__avløser2020').innerHTML = `<div class="lab-til__avløser2020">Hvorav avløsertilskudd 2021: <span>${numberWithCommas(data.entries[0].avloesertilskudd)} kr</span></div>`;
     }
   });
   e.classList.toggle("open")
 }
 
 
-async function get2020(e, orgnr) {
+async function get2021(e, orgnr) {
   let result = await jQuery.ajax({
     method: 'GET',
-    url: "https://hotell.difi.no/api/json/ldir/produksjon-og-avlosertilskudd/2020?query=" + orgnr,
+    url: "https://hotell.difi.no/api/json/ldir/produksjon-og-avlosertilskudd/2021?query=" + orgnr,
     async: true,
     success: function (response, text, request) { }
 
